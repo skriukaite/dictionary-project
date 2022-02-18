78c3b92000b40ef0d9eba62c89f6590f44df9064
skriukaite <skriukaite@gmail.com>
2022-02-18 10:36:13 +0200

    Add nes Results component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [searchedWord, setSearchedWord] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -29,6 +32,7 @@ export default function Dictionary() {
           onChange={handleEnteredWord}
         />{" "}
       </form>
+      <Results results={results} />
     </div>
   );
 }
