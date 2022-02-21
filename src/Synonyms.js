eafdad2cb665e5b2d75ec498ca8325d10e56fd81
skriukaite <skriukaite@gmail.com>
2022-02-21 11:51:36 +0200

    Add new Synonyms component

---
+++ src/Synonyms.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Synonyms(props) {
+  console.log(props.synonyms);
+  if (props.synonyms && props.synonyms.length > 0) {
+    return (
+      <div className="Synonyms">
+        <span>Synonyms:</span>
+        <ul>
+          {props.synonyms.map(function (synonym, index) {
+            return (
+              <li key={index}>
+                <span>{synonym}</span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
