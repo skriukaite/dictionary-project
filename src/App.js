7c5973371800813f0f2ba68ce4d00e79b1d11f00
skriukaite <skriukaite@gmail.com>
2022-02-28 14:26:22 +0200

    Update styling

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <div className="App-background">
-          <header className="App-header">DIC•TIO•NA•RY</header>
+      <div className="App-background">
+        <div className="container">
+          <header>DIC•TIO•NA•RY</header>
           <main>
-            <Dictionary defaultKeyword="book" />
+            <Dictionary defaultKeyword="peace" />
           </main>
           <footer className="text-left">
-            <small>Coded by Sandra Kriukaitė</small>
+            <small>
+              Coded by Sandra Kriukaitė, open-sourced on{" "}
+              <a
+                className="GitHub"
+                href="https://github.com/skriukaite/dictionary-project"
+                target="__blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+            </small>
           </footer>
         </div>
       </div>
